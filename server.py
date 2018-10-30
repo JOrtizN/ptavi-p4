edf7973a91f22fdc5b8029bc089b98430e8dd08d
Julia Ortiz Naharro <jortizna@beta05.aulas.gsyc.urjc.es>
2018-10-30 15:13:10 +0100

    servidor bien6

--- server.py
+++ server.py
@@ -11,34 +11,36 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    list_registers = []
+    dicc_registers = {}
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-
+        IP = self.client_address[0]
         print("El cliente nos manda:")
+
         for line in self.rfile:
-            print("line:", line.decode('utf-8'))
+            mensaje = line.decode('utf-8').split(" ")
+            #print("JULIA:", mensaje)
             if (line.decode('utf-8')[0:8] == "REGISTER"):
-                list_registers = [self.client_address[0],line.decode('utf-8')[9:]]
+                self.dicc_registers[mensaje[1].split(':')[-1]] = [IP]
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                print ("EN register:", line.decode('utf-8'))
-                print (list_registers)
+                #print ("EN register:", line.decode('utf-8'))
+                print (self.dicc_registers)
 
             elif (line == b'\r\n'):
                 continue
 
             else:
-                print(self.client_address[0], "\t",self.client_address[1], "\t", line.decode('utf-8'))
+                print(IP, "\t",self.client_address[1], "\t", line.decode('utf-8'))
                 self.wfile.write(line)
 
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), SIPRegisterHandler)
+    serv = socketserver.UDPServer(('', 6002), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
